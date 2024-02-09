1648a309640ef5bd7b4f2fa54469d1a0dd76521d
Radubtw <118615779+Radubtw@users.noreply.github.com>
2024-02-10 00:51:11 +0200

    Delete C-TestingFramework/C-Testing-Framework directory

--- C-TestingFramework/C-Testing-Framework/source/file2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello from  file2.c\n");
-    return 0;
-}
--- C-TestingFramework/C-Testing-Framework/source/source2/file1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello from file1.c\n")
-    return 0;
-}
